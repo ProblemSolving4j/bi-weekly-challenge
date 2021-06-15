031aad010d7b4287945e20c6e30d7a1bfdf15c60
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-06-15 12:38:04 +0300

    added implementation of MinWindowWithAllUniqueElementsProblem

--- src/main/java/com/softserveinc/ps4j/challenge/round001/MinWindowWithAllUniqueElementsProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round001/MinWindowWithAllUniqueElementsProblem.java
@@ -1,6 +1,6 @@
 package com.softserveinc.ps4j.challenge.round001;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * For a given list, find its smallest possible sublist that contains all of its unique elements.
@@ -11,7 +11,43 @@ import java.util.List;
 class MinWindowWithAllUniqueElementsProblem {
 
     <T> List<T> solve(List<T> list) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
 
+        if (list.size() == 0) return List.of();
+        if (list.size() == 1) return list;
+
+        var uniqueElements = new HashSet<>(list);
+        if (uniqueElements.size() == 1) {
+            return (List<T>) Arrays.asList(uniqueElements.toArray());
+        }
+        var minWindow = list;
+        var uniqueElementsEncountered = new HashSet<T>();
+
+        for (var i = 1; i < list.size(); i++) {
+
+            if ((list.get(i) == null && list.get(i - 1) != null)
+                    || (list.get(i) != null && list.get(i - 1) == null)
+                    || !list.get(i).equals(list.get(i - 1))) {
+
+                var j = i;
+                var window = new ArrayList<T>();
+
+                window.add(list.get(i - 1));
+                uniqueElementsEncountered.add(list.get(i - 1));
+
+                while (uniqueElementsEncountered.size() < uniqueElements.size()) {
+
+                    window.add(list.get(j));
+                    uniqueElementsEncountered.add(list.get(j));
+                    j++;
+                    if (j >= list.size()) break;
+                }
+
+                if (uniqueElementsEncountered.size() == uniqueElements.size() && window.size() < minWindow.size()) minWindow = window;
+            }
+
+            uniqueElementsEncountered.clear();
+        }
+
+        return minWindow;
+    }
 }
