02fbfa72117cca2ddee28a6b302a39e7e0d43f0c
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-06-16 12:55:04 +0300

    added solution for CostOfLinkingTrainsProblem

--- src/main/java/com/softserveinc/ps4j/challenge/round001/CostOfLinkingTrainsProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round001/CostOfLinkingTrainsProblem.java
@@ -1,5 +1,8 @@
 package com.softserveinc.ps4j.challenge.round001;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * We need to link the trains with different numbers of carts into one train.
  * The cost of linking two trains is equal to the sum of the amount of their carts.
@@ -8,7 +11,28 @@ package com.softserveinc.ps4j.challenge.round001;
 class CostOfLinkingTrainsProblem {
 
     int solve(int[] trains) {
-        throw new UnsupportedOperationException("not yet implemented");
+
+        if (trains.length == 0) {
+            return 0;
+        }
+        if (trains.length == 1) {
+            return trains[0];
+        }
+        if (trains.length == 2) {
+            return IntStream.of(trains).sum();
+        }
+
+        Arrays.sort(trains); // as far as sums are accumulating the least cost will be if we start connecting shortest trains first
+
+        int newTrain = trains[0] + trains [1];
+        int cost = newTrain;
+
+        for (var i = 2; i < trains.length; i++) {
+            newTrain += trains[i];
+            cost += newTrain;
+        }
+
+        return cost;
     }
 
 }
