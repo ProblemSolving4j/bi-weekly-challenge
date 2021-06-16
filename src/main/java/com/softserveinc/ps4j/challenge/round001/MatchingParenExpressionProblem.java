778ba6060368473ea451df725918caaeec0d60bc
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-06-16 12:57:58 +0300

    added solution for MatchingParenExpressionProblemTest

--- src/main/java/com/softserveinc/ps4j/challenge/round001/MatchingParenExpressionProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round001/MatchingParenExpressionProblem.java
@@ -1,5 +1,9 @@
 package com.softserveinc.ps4j.challenge.round001;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an expression string, examine whether the pairs and the orders of
  * “{“, “}”, “(“, “)”, “[“, “]” are correct.
@@ -7,9 +11,51 @@ package com.softserveinc.ps4j.challenge.round001;
  * Any non-paren symbols should be ignored.
  */
 class MatchingParenExpressionProblem {
-
-    boolean solve(String s) {
-        throw new UnsupportedOperationException("not yet implemented");
+    private static final Map<Character, Character> SYMBOL_MATCHING = new HashMap<>();
+    static {
+        SYMBOL_MATCHING.put('{', '}');
+        SYMBOL_MATCHING.put('[', ']');
+        SYMBOL_MATCHING.put('(', ')');
     }
 
+    boolean solve(String s) {
+
+        var ch = (char) 0;
+        var expressions = new ArrayList<Expression>();
+
+        for (var i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            switch (ch) {
+                case '[':
+                case '{':
+                case '(':
+                    expressions.add(new Expression(ch));
+                    break;
+                case ']':
+                case '}':
+                case ')':
+                    if (expressions.size() == 0
+                            || !expressions.get(expressions.size() - 1).closeWith(ch)) {
+                        return false;
+                    } else {
+                        expressions.remove(expressions.size() - 1);
+                    }
+                    break;
+            }
+        }
+
+        return expressions.size() == 0;
+    }
+
+    private class Expression {
+        char openingSymbol;
+
+        private Expression (char openingSymbol) {
+            this.openingSymbol = openingSymbol;
+        }
+
+        private boolean closeWith (char closingSymbol) {
+            return closingSymbol == SYMBOL_MATCHING.get(openingSymbol);
+        }
+    }
 }
