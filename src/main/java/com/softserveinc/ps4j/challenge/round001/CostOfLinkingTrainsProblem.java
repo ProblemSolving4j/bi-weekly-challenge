1c97e5fe1cf47e04fc35f844b5e27e81ad4cccfb
Ievgen <ievgen.asmolov@bazaarvoice.com>
2021-06-21 23:33:10 +0300

    sorry not the best solution, but time is up and here is my answer

--- src/main/java/com/softserveinc/ps4j/challenge/round001/CostOfLinkingTrainsProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round001/CostOfLinkingTrainsProblem.java
@@ -19,7 +19,16 @@ class CostOfLinkingTrainsProblem {
         Arrays.sort(trains);
         int lastCart = trainSize = trains[0] + trains[1];
         for (int i=2; i < trains.length; i++) {
-            lastCart = lastCart + trains[i];
+            if (trains[i] >= lastCart) {
+                lastCart = lastCart + trains[i];
+                //when we have few trains with the same amount of carts we need to have a look in advance that they are combined
+            } else if (i + 1 < trains.length && trains[i] == trains[i+1]) {
+                trainSize += trains[i] + trains[i+1];
+                lastCart = lastCart + trains[i] + trains[i+1];
+                i++; // we already processed next train
+            } else {
+                lastCart = lastCart + trains[i];
+            }
             trainSize += lastCart;
         }
         return trainSize;
