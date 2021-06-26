073d3daa3cb4cb9a5e73768391be50aac5573c0d
Hanna Yanchevska <hyanchevska@atlassian.com>
2021-06-26 18:10:02 +0300

    PascalsTriangleProblem solution

--- src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
@@ -1,12 +1,15 @@
 package com.softserveinc.ps4j.challenge.round002;
 
+import java.util.Arrays;
+
 /**
  * Pascal's triangle is triangular matrix that looks like this:
  * <pre>
  * row 0:        1
- * row 1:      1 2 1
- * row 2:     1 3 3 1
- * row 3:    1 4 6 4 1
+ * row 1:       1 1
+ * row 2:      1 2 1
+ * row 3:     1 3 3 1
+ * row 4:    1 4 6 4 1
  * ...
  * </pre>
  * <p>Each value in a row is a sum of two values from the previous row.
@@ -15,8 +18,16 @@ package com.softserveinc.ps4j.challenge.round002;
  */
 class PascalsTriangleProblem {
 
-    int[] solve(int row) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
+    int[] solve(int rowNumber) {
+        if (rowNumber == 0) return new int[]{1};
+        int[] row = new int[rowNumber + 2];
+        row[0] = 1;
 
+        for (int i = 1; i <= rowNumber; i++) {
+            for (int j = i; j >= 1; j--) {
+                row[j] += row[j - 1];
+            }
+        }
+        return Arrays.copyOfRange(row, 0, rowNumber + 1);
+    }
 }
