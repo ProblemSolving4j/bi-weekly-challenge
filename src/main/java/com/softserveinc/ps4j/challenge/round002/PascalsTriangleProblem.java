db5bf00f083eee508804c9277f8a0abba4847565
Mikhail Horbunov <miwagorbi@gmail.com>
2021-06-30 18:03:27 +0300

    ps4j002: PascalsTriangleProblem doc fix

--- src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
@@ -4,9 +4,10 @@ package com.softserveinc.ps4j.challenge.round002;
  * Pascal's triangle is triangular matrix that looks like this:
  * <pre>
  * row 0:        1
- * row 1:      1 2 1
- * row 2:     1 3 3 1
- * row 3:    1 4 6 4 1
+ * row 1:       1 1
+ * row 2:      1 2 1
+ * row 3:     1 3 3 1
+ * row 4:    1 4 6 4 1
  * ...
  * </pre>
  * <p>Each value in a row is a sum of two values from the previous row.
