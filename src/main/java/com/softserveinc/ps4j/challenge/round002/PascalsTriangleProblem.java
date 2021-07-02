592c397faa773ca24439ee39e5e149a6f7296bbf
kovch <kovch@softserveinc.com>
2021-07-02 23:09:12 +0300

    kovcharenko-ps4j002: Tasks solutions update.

--- src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round002/PascalsTriangleProblem.java
@@ -4,9 +4,10 @@ package com.softserveinc.ps4j.challenge.round002;
  * Pascal's triangle is triangular matrix that looks like this:
  * <pre>
  * row 0:        1
- * row 1:      1 2 1
- * row 2:     1 3 3 1
- * row 3:    1 4 6 4 1
+ * row 1:       1 1
+ * row 2:      1 2 1
+ * row 3:     1 3 3 1
+ * row 4:    1 4 6 4 1
  * ...
  * </pre>
  * <p>Each value in a row is a sum of two values from the previous row.
@@ -16,6 +17,7 @@ package com.softserveinc.ps4j.challenge.round002;
 class PascalsTriangleProblem {
 
     int[] solve(int row) {
+        if (row < 0) throw new IllegalArgumentException("row >=0");
         int[] result = row == 0 ? new int[]{1} : row == 1 ? new int[]{1, 1} : null;
         int[] base = {1, 1};
         if (row > 1) {
