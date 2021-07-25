8bae1e9a867cb6c62961eda04824f6a8a9ef5bb7
Mikhail Horbunov <miwagorbi@gmail.com>
2021-07-25 16:30:50 +0300

    ps4j004: problems & tests

---
+++ src/main/java/com/softserveinc/ps4j/challenge/round004/HouseRobberProblem.java
@@ -0,0 +1,19 @@
+package com.softserveinc.ps4j.challenge.round004;
+
+/**
+ * You are a professional robber planning to rob houses along a street.
+ * <p>Each house has a certain amount of money stashed,
+ * the only constraint stopping you from robbing each of them is that
+ * adjacent houses have security systems connected and it will
+ * automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * <p>Given an integer array representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ */
+class HouseRobberProblem {
+
+    int solve(int... houses) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+}
