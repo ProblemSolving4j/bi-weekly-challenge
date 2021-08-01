85c973850291fb22a99ccb9a5bd26ecfb98dbafa
Hanna Yanchevska <hyanchevska@atlassian.com>
2021-08-01 15:48:50 +0300

    round004: LFU cache solution

--- src/main/java/com/softserveinc/ps4j/challenge/round004/LFUCacheProblem.java
+++ src/main/java/com/softserveinc/ps4j/challenge/round004/LFUCacheProblem.java
@@ -1,17 +1,135 @@
 package com.softserveinc.ps4j.challenge.round004;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Design a data structure that will implement {@link LFUCache}.
  */
 class LFUCacheProblem {
 
     <K, V> LFUCache<K, V> solve(int capacity) {
-        if (capacity <= 0) {
-            throw new IllegalArgumentException("LFU cache capacity should be positive, got " + capacity);
+        {
+            if (capacity <= 0) {
+                throw new IllegalArgumentException("LFU cache capacity should be positive, got " + capacity);
+            }
+            return new LFUCacheProblemImpl<>(capacity);
         }
-        throw new UnsupportedOperationException("not yet implemented");
     }
 
+    private static class LFUCacheProblemImpl<K, V> implements LFUCache<K, V> {
+
+        private final int capacity;
+        private HashMap<K, ValueWithFrequency<V>> valMap;
+        private int minFrequency;
+        private HashMap<Integer, Deque<K>> frequencyMap;
+
+        public LFUCacheProblemImpl(int capacity) {
+            this.capacity = capacity;
+            this.valMap = new HashMap<>();
+            this.frequencyMap = new HashMap<>();
+            this.minFrequency = 0;
+        }
+
+        @Override
+        public V get(K key) {
+            if (!valMap.containsKey(key)) {
+                return null;
+            }
+            ValueWithFrequency<V> valueWithFrequency = valMap.get(key);
+            int freq = valueWithFrequency.getFrequency();
+            V value = valueWithFrequency.getValue();
+            updateFrequencyMap(key, freq);
+            valMap.put(key, new ValueWithFrequency<>(value, freq + 1));
+            return value;
+        }
+
+        @Override
+        public V put(K key, V value) {
+            if (!valMap.containsKey(key)) {
+                if (valMap.size() >= capacity) {
+                    Deque<K> keysWithMinFrequency = frequencyMap.get(minFrequency);
+                    K evict = keysWithMinFrequency.pollFirst();
+                    valMap.remove(evict);
+                    if (keysWithMinFrequency.size() == 0) {
+                        frequencyMap.remove(minFrequency);
+                    }
+                }
+                valMap.put(key, new ValueWithFrequency<>(value, 1));
+                addToFrequencyMap(key, 1);
+                minFrequency = 1;
+                return null;
+            } else {
+                ValueWithFrequency<V> prevValue = valMap.get(key);
+                int freq = prevValue.getFrequency();
+                valMap.put(key, new ValueWithFrequency<>(value, freq + 1));
+                return prevValue.getValue();
+            }
+        }
+
+        @Override
+        public int capacity() {
+            return capacity;
+        }
+
+        @Override
+        public int size() {
+            return valMap.size();
+        }
+
+        private static class ValueWithFrequency<V> {
+            private final int frequency;
+            private final V value;
+
+            public ValueWithFrequency(V value, int frequency) {
+                this.frequency = frequency;
+                this.value = value;
+            }
+
+            public int getFrequency() {
+                return frequency;
+            }
+
+            public V getValue() {
+                return value;
+            }
+        }
+
+        private void updateFrequencyMap(K key, int freq) {
+            Deque<K> keys = frequencyMap.get(freq);
+            if (keys.size() == 1) {
+                frequencyMap.remove(freq);
+                if (minFrequency == freq) {
+                    minFrequency++;
+                }
+            } else {
+                keys.remove(key);
+            }
+            Deque<K> keysNextFreq = frequencyMap.get(freq + 1);
+            if (keysNextFreq == null) {
+                Deque<K> keysN = new LinkedList<>();
+                keysN.add(key);
+                frequencyMap.put(freq + 1, keysN);
+            } else {
+                keysNextFreq.addLast(key);
+                frequencyMap.put(freq + 1, keysNextFreq);
+            }
+        }
+
+        private void addToFrequencyMap(K key, int freq) {
+            Deque<K> keys = frequencyMap.get(freq);
+            if (keys == null) {
+                Deque<K> keysN = new LinkedList<>();
+                keysN.add(key);
+                frequencyMap.put(freq, keysN);
+            } else {
+                keys.add(key);
+                frequencyMap.put(freq, keys);
+            }
+        }
+
+    }
 }
 
 /**
